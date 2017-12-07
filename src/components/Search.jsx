ec41394266560821c0697354518215302d022185
jdbyrd <jd3yrd@gmail.com>
2017-12-07 12:20:31 -0600

    Added ability to press enter key on search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,10 +3,18 @@ class Search extends React.Component {
     super(props);
   }
   
+  handleKeyPress(event) {
+    if (event.charCode === 13) {
+      // event.preventDefault();
+      // event.stopPropagation();
+      return this.props.onEnter($('.form-control').val());
+    }
+  }
+  
   render () {
     return (
       <div className="search-bar form-inline">
-        <input className="form-control" type="text" />
+        <input onKeyPress={(event) => this.handleKeyPress(event)} className="form-control" type="text" />
         <button onClick={() => this.props.onEnter($('.form-control').val())} className="btn hidden-sm-down">
           <span className="glyphicon glyphicon-search"></span>
         </button>
